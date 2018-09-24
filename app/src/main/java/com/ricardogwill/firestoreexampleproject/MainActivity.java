3c6d1bf0609b53086552aaf7fb7622d19fd1603a
Ricardo Willems <RicardoGWill@gmail.com>
2018-09-24 01:20:05 -0400

    The "Load Note" button has become superfluous, and I removed its function at this point, because now once the "Save Note" Button is pressed, the note at the bottom is automatically updated.  An "Update Description" Button has been added, and this changes the description that is typed in as long as a note has already been saved and is on Firestore.  If there is no note, then the "Update Description" Button will not work, even if the Description "EditText" has text in it.

--- app/src/main/java/com/ricardogwill/firestoreexampleproject/MainActivity.java
+++ app/src/main/java/com/ricardogwill/firestoreexampleproject/MainActivity.java
@@ -1,6 +1,7 @@
 package com.ricardogwill.firestoreexampleproject;
 
 import android.content.Context;
+import android.media.audiofx.AudioEffect;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -15,10 +16,15 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.SetOptions;
 
 import java.util.HashMap;
 import java.util.Map;
+
+import javax.annotation.Nullable;
 
 // This project is based off the tutorial here: https://www.youtube.com/watch?v=MILE4PVx1kE&index=2&list=PLrnPJCHvNZuDrSqu-dKdDi3Q6nM-VUyxD
 public class MainActivity extends AppCompatActivity {
@@ -36,6 +42,7 @@ public class MainActivity extends AppCompatActivity {
     // Note that "firebaseFirestore.collection("Notebook/My First Note");" is equal to the right half below.
     private DocumentReference documentReference = firebaseFirestore.collection("Notebook").document("My First Note");
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,6 +53,31 @@ public class MainActivity extends AppCompatActivity {
         dataTextView = findViewById(R.id.data_textView);
 
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Using "this" before the new EventListener DETATCHES the listener at the appropriate moment.
+        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
+                if (e != null) {
+                    Toast.makeText(MainActivity.this, "Error while loading.", Toast.LENGTH_SHORT).show();
+                    // Just type "logd" for the basic template for the line below.
+                    Log.d(TAG, e.toString());
+                    return;
+                }
+                // This "if" statement is nearly the same as in the "loadNote" method below.
+                if (documentSnapshot.exists()) {
+                    String title = documentSnapshot.getString(KEY_TITLE);
+                    String description = documentSnapshot.getString(KEY_DESCRIPTION);
+
+                    dataTextView.setText("Title: " + title + "\n" + "Description: " + description);
+                }
+            }
+        });
+    }
+
     // "OnClick" XML is used instead of an OnClickListener.
     public void saveNote(View view) {
         String titleString = titleEditText.getText().toString();
@@ -73,29 +105,46 @@ public class MainActivity extends AppCompatActivity {
                 });
     }
 
-    public void loadNote(View view) {
-        documentReference.get()
-                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-                    @Override
-                    public void onSuccess(DocumentSnapshot documentSnapshot) {
-                        if (documentSnapshot.exists()) {
-                            String title = documentSnapshot.getString(KEY_TITLE);
-                            String description = documentSnapshot.getString(KEY_DESCRIPTION);
+//    public void loadNote(View view) {
+//        documentReference.get()
+//                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+//                    @Override
+//                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+//                        if (documentSnapshot.exists()) {
+//                            String title = documentSnapshot.getString(KEY_TITLE);
+//                            String description = documentSnapshot.getString(KEY_DESCRIPTION);
+//
+//                            // Map<String, Object> note = documentSnapshot.getData();
+//                            dataTextView.setText("Title: " + title + "\n" + "Description: " + description);
+//                        } else {
+//                            Toast.makeText(MainActivity.this, "Document does not exist.", Toast.LENGTH_SHORT).show();
+//                        }
+//                    }
+//                })
+//                .addOnFailureListener(new OnFailureListener() {
+//                    @Override
+//                    public void onFailure(@NonNull Exception e) {
+//                        Toast.makeText(MainActivity.this, "Error!", Toast.LENGTH_SHORT).show();
+//                        Log.d(TAG, e.toString());
+//                    }
+//                });
+//    }
 
-                            // Map<String, Object> note = documentSnapshot.getData();
-                            dataTextView.setText("Title: " + title + "\n" + "Description: " + description);
-                        } else {
-                            Toast.makeText(MainActivity.this, "Document does not exist.", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(MainActivity.this, "Error!", Toast.LENGTH_SHORT).show();
-                        Log.d(TAG, e.toString());
-                    }
-                });
+    public void updateDescription(View view) {
+        String description = descriptionEditText.getText().toString();
+
+        // Note: The below notes show ONE way to do this, if we do "documentReference.set" instead of ".update".
+        // Note: Adding "SetOptions.merge()" makes it so that updating the description does not make the title "null",
+        // Note cont'd: but rather leaves the title as it is.
+
+        // Map<String, Object> note = new HashMap<>();
+        // note.put(KEY_DESCRIPTION, description);
+        // documentReference.set(note, SetOptions.merge());
+
+        // Note: "documentReference.update" just updates the description but doesn't delete the title.
+        // Note: Unlike the commented out code above, ".update" does not do anything if no document exists already on Firestore.
+        documentReference.update(KEY_DESCRIPTION, description);
+        removeKeyboardAndMakeEditTextBlank();
     }
 
     public void removeKeyboardAndMakeEditTextBlank() {
