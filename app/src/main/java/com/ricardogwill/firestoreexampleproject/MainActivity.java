a5300859063358ec9157a56422ece5f9f37d7a6c
Ricardo Willems <RicardoGWill@gmail.com>
2018-09-25 15:53:37 -0400

    This introduced Batch writes.  (It will be replaced by "transactions" in the next update.

--- app/src/main/java/com/ricardogwill/firestoreexampleproject/MainActivity.java
+++ app/src/main/java/com/ricardogwill/firestoreexampleproject/MainActivity.java
@@ -2,28 +2,23 @@ package com.ricardogwill.firestoreexampleproject;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.android.gms.tasks.Tasks;
 import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
-
-import java.util.List;
-
-import javax.annotation.Nullable;
+import com.google.firebase.firestore.WriteBatch;
 
 // This project is based off the tutorial here: https://www.youtube.com/watch?v=MILE4PVx1kE&index=2&list=PLrnPJCHvNZuDrSqu-dKdDi3Q6nM-VUyxD
 public class MainActivity extends AppCompatActivity {
@@ -46,39 +41,41 @@ public class MainActivity extends AppCompatActivity {
         priorityEditText = findViewById(R.id.priority_editText);
         dataTextView = findViewById(R.id.data_textView);
 
+        executeTransaction();
+
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        collectionReference.addSnapshotListener(this, new EventListener<QuerySnapshot>() {
-            @Override
-            public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
-                if (e != null) {
-                    return;
-                }
-
-                for (DocumentChange documentChange : queryDocumentSnapshots.getDocumentChanges()) {
-                    DocumentSnapshot documentSnapshot = documentChange.getDocument();
-                    String id = documentSnapshot.getId();
-                    int oldIndex  = documentChange.getOldIndex();
-                    int newIndex = documentChange.getNewIndex();
-
-                    switch (documentChange.getType()) {
-                        case ADDED:
-                            dataTextView.append("\nAdded: " + id + "\nOld Index: " + oldIndex + "\nNew Index: " + newIndex);
-                            break;
-                        case MODIFIED:
-                            dataTextView.append("\nModified: " + id + "\nOld Index: " + oldIndex + "\nNew Index: " + newIndex);
-                            break;
-                        case REMOVED:
-                            dataTextView.append("\nRemoved: " + id + "\nOld Index: " + oldIndex + "\nNew Index: " + newIndex);
-                            break;
-                    }
-                }
-            }
-        });
-    }
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//        collectionReference.addSnapshotListener(this, new EventListener<QuerySnapshot>() {
+//            @Override
+//            public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
+//                if (e != null) {
+//                    return;
+//                }
+//
+//                for (DocumentChange documentChange : queryDocumentSnapshots.getDocumentChanges()) {
+//                    DocumentSnapshot documentSnapshot = documentChange.getDocument();
+//                    String id = documentSnapshot.getId();
+//                    int oldIndex  = documentChange.getOldIndex();
+//                    int newIndex = documentChange.getNewIndex();
+//
+//                    switch (documentChange.getType()) {
+//                        case ADDED:
+//                            dataTextView.append("\nAdded: " + id + "\nOld Index: " + oldIndex + "\nNew Index: " + newIndex);
+//                            break;
+//                        case MODIFIED:
+//                            dataTextView.append("\nModified: " + id + "\nOld Index: " + oldIndex + "\nNew Index: " + newIndex);
+//                            break;
+//                        case REMOVED:
+//                            dataTextView.append("\nRemoved: " + id + "\nOld Index: " + oldIndex + "\nNew Index: " + newIndex);
+//                            break;
+//                    }
+//                }
+//            }
+//        });
+//    }
 
     // "OnClick" XML is used instead of an OnClickListener.
     public void addNote(View view) {
@@ -136,6 +133,28 @@ public class MainActivity extends AppCompatActivity {
                         }
                     }
                 });
+    }
+
+    public void executeTransaction() {
+        WriteBatch batch = firebaseFirestore.batch();
+        DocumentReference doc1 = collectionReference.document("New Note");
+        batch.set(doc1, new Note("New Note", "New Note", 1));
+
+        DocumentReference doc2 = collectionReference.document("4kc6Pu4zIKZsuv5xpeEQ");
+        batch.update(doc2, "title", "Updated Note");
+
+        DocumentReference doc3 = collectionReference.document("Z5G0JwvQp6Beu1zEyt7k");
+        batch.delete(doc3);
+
+        DocumentReference doc4 = collectionReference.document();
+        batch.set(doc4, new Note("Added Note", "Added Note", 1));
+
+        batch.commit().addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                dataTextView.setText(e.toString());
+            }
+        });
     }
 
 
